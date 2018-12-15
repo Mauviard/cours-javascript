b17dc3fd83c75706deebf193e12e72b57ee545e1
Mauviard <45900145+Mauviard@users.noreply.github.com>
2018-12-15 16:57:00 +0100

    Add files via upload

---
+++ exemple.js
@@ -0,0 +1,2 @@
+// 'bonjour' = afficher la chaine de caractère qui se trouve dans les ''
+for (var i = 0; i < 6; i++) {console.log(i);}
---
+++ main.js
@@ -0,0 +1,19 @@
+
+
+for (let i = 0; i < 6; i++) {
+  console.log(i);
+  if(i<3) {
+    console.log('bonjour')
+
+  }
+  else{
+    console.log('au revoir')
+  }
+}
+
+let i = 0;
+while (i == 0) {
+  console.log(i)
+  i = i+1
+  console.log(i)
+}
